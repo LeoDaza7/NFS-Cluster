1a5e17cc2a40172416779e2a55d7c16c3da421a5
Leo <leo.daza.769@gmail.com>
2019-06-16 22:50:02 -0400

    build_deploy_nfs-client.sh updated

--- build_deploy_nfs-client.sh
+++ build_deploy_nfs-client.sh
@@ -3,5 +3,6 @@
 docker build -t nfs-client .
 docker swarm init
 sudo mkdir /nfs-client-volume
+sudo apt install nfs-common
 sudo mount $1:/ /nfs-client-volume
 docker stack deploy -c docker-compose.yml nfs-cluster
